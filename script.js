dd0716cee08fcb62a49aae2f5d89a51591815dfe
oybek003 <ojbekmuslimov1@gmail.com>
2024-12-24 15:23:11 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+// let person = {
+    // name: "Ivan",
+    // age: 30,
+    // isStudent: false
+// };
+// 
+// console.log(person.name);
+// console.log(person.age);
+// 
+// person.city = "NewYork";
+// person.age = 26;
+// 
+// delete person.isStudent;
+// 
+// 
+
+// let film = {
+    // title: "Avangers",
+    // director: "Oybek",
+    // releaseYear: 1999,
+    // genres: "komedi",
+    // isAwardWinner: "yes"
+// };
+// 
+// console.log(film);\
+
+
+let ot = prompt("ismingizni kiriting")
+
+let space = prompt("space di idni kiriting")
+
+let password = prompt("space dagi passwordingizi kiriting")
+
+let all = {
+    name: `${ot}`,
+    id: `${space}`,
+    password: `${password}`
+};
+
+console.log(all);
